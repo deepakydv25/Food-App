cb863ddf1d7e6dc15f83d0d89b6188766255950e
Deepak Yadav <deepaksyadav00@gmail.com>
2023-01-21 19:58:47 +0530

    Header added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,35 @@
 import React from "react";
+import Logo from "../assets/img/foodvilla.png";
+import { Link } from "react-router-dom";
+
+const Title = () => (
+  <a href="/">
+    <img className="logo" alt="logo" src={Logo} />
+  </a>
+);
 
 const Header = () => {
-  return <h1>Header component</h1>;
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="cart">Cart</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
