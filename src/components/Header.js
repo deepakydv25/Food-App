990bba0ec025f82e2931a74772b6bf52937f4e3c
Deepak Yadav <deepaksyadav00@gmail.com>
2023-01-31 23:25:54 +0530

    Header refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Logo from "../assets/img/foodvilla.png";
 import { Link } from "react-router-dom";
-import SignupForm from "./SignUpForm";
+// import SignupForm from "./SignUpForm";
 import useOnline from "../utils/useOnline";
 
 const Title = () => (
